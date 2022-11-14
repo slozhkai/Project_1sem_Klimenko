22d522b7dab5e4fa35fbbf29119161e170df495c
slozhkai <114486246+slozhkai@users.noreply.github.com>
2022-11-14 22:28:47 +0300

    Update PZ_6_1_Klimenko.py

--- PZ_6/PZ_6_1_Klimenko.py
+++ PZ_6/PZ_6_1_Klimenko.py
@@ -6,16 +6,19 @@
 
 import random
 
-a = []
-n = random.randint(1, 20)
-k = random.randrange(1, n)
-i = 0
-while i < n:
-    a.append(random.randint(0, 10))
-    i += 1
+try:
+    a = []
+    n = random.randint(1, 20)
+    k = random.randrange(1, n)
+    i = 0
+    while i < n:
+        a.append(random.randint(0, 10))
+        i += 1
 
-for s in range((k - 1), n, k):
-    print("Элемент:", a[s], "Порядковый номер:", s + 1)
+    for s in range((k - 1), n, k):
+        print("Элемент:", a[s], "Порядковый номер:", s + 1)
+except:
+    print("Некорректно введеные данные")
 
 
 
